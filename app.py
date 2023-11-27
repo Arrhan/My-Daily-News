cbd7038f1b269a633410b01c21efc583db02490e
Arrhan <arrhan.bhatia@gmail.com>
2023-11-26 22:02:41 -0600

    Deploy

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def index():
     return render_template('index.html', Rlinks=Rlinks, Mlinks=Mlinks, Tlinks=Tlinks, TTlinks=TTlinks)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
